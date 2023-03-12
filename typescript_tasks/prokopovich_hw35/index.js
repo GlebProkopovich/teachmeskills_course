209c191c38988f241c5c130d67f38da98462ebab
Gleb Prokopovich <g.prokopovich@rambler.ru>
2023-03-12 14:16:18 +0400

    refactor(hw_35): added functions and brackets

--- typescript_tasks/prokopovich_hw35/index.js
+++ typescript_tasks/prokopovich_hw35/index.js
@@ -1,41 +1,48 @@
 "use strict";
 const users = [
-  {
-    name: "Harry Felton",
-    phone: "(09) 897 33 33",
-    email: "felton@gmail.com",
-    animals: ["cat"],
-    cars: ["bmw"],
-    hasChildren: false,
-    hasEducation: true
-  },
-  {
-    name: "May Sender",
-    phone: "(09) 117 33 33",
-    email: "sender22@gmail.com",
-    hasChildren: true,
-    hasEducation: true
-  },
-  {
-    name: "Henry Ford",
-    phone: "(09) 999 93 23",
-    email: "ford0@gmail.com",
-    cars: ["bmw", "audi"],
-    hasChildren: true,
-    hasEducation: false
-  }
+    {
+        name: 'Harry Felton',
+        phone: '(09) 897 33 33',
+        email: 'felton@gmail.com',
+        animals: ['cat'],
+        cars: ['bmw'],
+        hasChildren: false,
+        hasEducation: true,
+    },
+    {
+        name: 'May Sender',
+        phone: '(09) 117 33 33',
+        email: 'sender22@gmail.com',
+        hasChildren: true,
+        hasEducation: true,
+    },
+    {
+        name: 'Henry Ford',
+        phone: '(09) 999 93 23',
+        email: 'ford0@gmail.com',
+        cars: ['bmw', 'audi'],
+        hasChildren: true,
+        hasEducation: false,
+    },
 ];
-users.map(i => i.name).join(', ');
-users.filter(i => i.cars).map(i => i.cars).reduce((acc, item) => acc + item.length, 0);
+function getNames(arr) {
+    return arr.map((el) => el.name).join(', ');
+}
+function getAmountCars(arr) {
+    return (arr
+        .filter((el) => el.cars)
+        .map((el) => el.cars)
+        .reduce((acc, item) => acc + item.length, 0));
+}
 function getUsersEducated(arr) {
-  return arr.filter(i => i.hasEducation);
+    return arr.filter((el) => el.hasEducation);
 }
 function getUsersWithAnimals(arr) {
-  return arr.filter(i => i.animals);
+    return arr.filter((el) => el.animals);
 }
 function getUsersCars(arr) {
-  return arr
-    .filter(i => i.cars)
-    .map(i => i.cars)
-    .join(',');
+    return (arr
+        .filter((el) => el.cars)
+        .map((el) => el.cars)
+        .join(','));
 }
--- typescript_tasks/prokopovich_hw35/index.ts
+++ typescript_tasks/prokopovich_hw35/index.ts
@@ -1,67 +1,76 @@
 interface IUser {
-  name: string,
-  phone: string,
-  email: string,
-  animals?: Array<string>,
-  cars?: Array<string>,
-  hasChildren: boolean,
-  hasEducation: boolean,
+  name: string;
+  phone: string;
+  email: string;
+  animals?: Array<string>;
+  cars?: Array<string>;
+  hasChildren: boolean;
+  hasEducation: boolean;
 }
 
 const users: Array<IUser> = [
   {
-      name: "Harry Felton",
-      phone: "(09) 897 33 33",
-      email: "felton@gmail.com",
-      animals: ["cat"],
-      cars: ["bmw"],
-      hasChildren: false,
-      hasEducation: true
-      
+    name: 'Harry Felton',
+    phone: '(09) 897 33 33',
+    email: 'felton@gmail.com',
+    animals: ['cat'],
+    cars: ['bmw'],
+    hasChildren: false,
+    hasEducation: true,
   },
   {
-      name: "May Sender",
-      phone: "(09) 117 33 33",
-      email: "sender22@gmail.com",
-      hasChildren: true,
-      hasEducation: true
+    name: 'May Sender',
+    phone: '(09) 117 33 33',
+    email: 'sender22@gmail.com',
+    hasChildren: true,
+    hasEducation: true,
   },
   {
-      name: "Henry Ford",
-      phone: "(09) 999 93 23",
-      email: "ford0@gmail.com",
-      cars: ["bmw", "audi"],
-      hasChildren: true,
-      hasEducation: false
-  }
-]
+    name: 'Henry Ford',
+    phone: '(09) 999 93 23',
+    email: 'ford0@gmail.com',
+    cars: ['bmw', 'audi'],
+    hasChildren: true,
+    hasEducation: false,
+  },
+];
 
 // 1. Создать строку из имен пользователей через запятую
 
-users.map(i => i.name).join(', ');
+function getNames(arr: Array<IUser>): string {
+  return arr.map((el) => el.name).join(', ');
+}
 
 // 2. Посчитать общее количнство машин у пользователей
 
-users.filter(i => i.cars).map(i => i.cars).reduce((acc, item) => acc + item!.length, 0);
+function getAmountCars(arr: Array<IUser>): number {
+  return ( 
+    arr
+    .filter((el) => el.cars)
+    .map((el) => el.cars)
+    .reduce((acc, item) => acc + item!.length, 0)
+  );
+}
 
 // 3. Создать функцию, которая бы принимала массив пользователей и отфильтровывала пользователей на наличие образования
 
 function getUsersEducated(arr: Array<IUser>): Array<IUser> {
-  return arr.filter(i => i.hasEducation);
+  return arr.filter((el) => el.hasEducation);
 }
 
 // 4. Создать функцию, которая бы принимала массив пользователей и отфильтровывала пользователей на наличие животных
 
 function getUsersWithAnimals(arr: Array<IUser>): Array<IUser> {
-  return arr.filter(i => i.animals);
+  return arr.filter((el) => el.animals);
 }
 
 // 5. Создать функцию, которая бы принимала массив пользователей и отдавала бы  строку с названиями марок автомобилей через запятую
 
 function getUsersCars(arr: Array<IUser>) {
-  return arr
-  .filter(i => i.cars)
-  .map(i => i.cars)
-  .join(',');
+  return ( 
+    arr
+    .filter((el) => el.cars)
+    .map((el) => el.cars)
+    .join(',')
+  );
 }
-
